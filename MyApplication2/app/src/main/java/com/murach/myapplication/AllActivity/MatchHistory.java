4adcc4f95ffc46c8bf4f4da2d3b48e0da4d9efe3
Minh Nguyễn <minhnguyen13864@gmail.com>
2024-06-07 17:10:25 +0700

    Update view to project

---
+++ MyApplication2/app/src/main/java/com/murach/myapplication/AllActivity/MatchHistory.java
@@ -0,0 +1,15 @@
+package com.murach.myapplication.AllActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.murach.myapplication.R;
+
+public class MatchHistory extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.match_history);
+    }
+}
