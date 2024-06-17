8f24c2be4c2bc4808c87619644337e7a4c8abff8
zhaduchanhzz <zhaduchanhzz@gmail.com>
2024-06-17 14:58:10 +0700

    complete server network

---
+++ ChessServer/src/main/java/org/example/ChessPiece.java
@@ -0,0 +1,52 @@
+package org.example;
+
+enum Player {
+    WHITE,
+    BLACK,
+}
+
+enum Rank {
+    KING,
+    QUEEN,
+    BISHOP,
+    ROOK,
+    KNIGHT,
+    PAWN,
+}
+
+public class ChessPiece {
+    private final int col;
+    private final int row;
+    private final Player player;
+    private final Rank rank;
+    private final String imgName;
+
+    public ChessPiece(int col, int row, Player player, Rank rank, String imgName) {
+        super();
+        this.col = col;
+        this.row = row;
+        this.player = player;
+        this.rank = rank;
+        this.imgName = imgName;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public String getImgName() {
+        return imgName;
+    }
+}
